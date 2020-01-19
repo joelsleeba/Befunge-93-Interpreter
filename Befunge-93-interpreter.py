317c1a82dc910440e71cfe04691a93c7eddff836
Joel Sleeba <54855387+joelsleeba1@users.noreply.github.com>
2020-01-20 00:22:53 +0530

    Update Befunge-93-interpreter.py

--- Befunge-93-interpreter.py
+++ Befunge-93-interpreter.py
@@ -190,3 +190,5 @@ interpret(    '>              v\nv  ,,,,,"Hello"<\n>48*,          v\nv,,,,,,"Wor
 interpret('08>:1-:v v *_$.@ \n  ^    _$>\:^')# output and input are same
 interpret('01->1# +# :# 0# g# ,# :# 5# 8# *# 4# +# -# _@')
 interpret('v@.<\n >1^\n>?<^\n >2^')
+#interpret('2>:3g" "-!v\  g30          <\n |!`"&":+1_:.:03p>03g+:"&"`|\n @               ^  p3\" ":<\n2 2345678901234567890123456789012345678')
+#interpret('222p35*89+*11p>133p                   >33g1+33p   22g33g- v>22g33g%#v_v\n o     
